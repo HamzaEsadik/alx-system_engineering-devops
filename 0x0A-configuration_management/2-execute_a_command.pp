a0f9bafb8734e909447c655145a943df3b588710
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-02-23 17:27:16 +0100

    task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 #manifest that kills a process named killmenow
 exec { 'killmenow':
-  commande => 'pkill killmenow'
-  path     => '/bin/'
+  commande => 'pkill killmenow',
+  path     => '/bin/',
 }
