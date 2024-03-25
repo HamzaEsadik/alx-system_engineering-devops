393ba49a24f626f58a1fd39f7538f11770a6c0c7
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-25 14:38:22 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''gather data from api'''
+import requests
+import sys
+
+url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+try:
+    # fetch data
+    user_response = requests.get(url)
+    # parse data
+    user_data = user_response.json()
+    # name
+    EMPLOYEE_NAME = user_data['name']
+except requests.RequestException as e:
+    print(e)
+
+all_todos_url = (
+    'https://jsonplaceholder.typicode.com/todos?userId=' +
+    sys.argv[1])
+try:
+    # fetch data
+    all_todos_response = requests.get(all_todos_url)
+    # parse data
+    all_todos_data = all_todos_response.json()
+    # TOTAL_NUMBER_OF_TASKS
+    TOTAL_NUMBER_OF_TASKS = len(all_todos_data)
+except requests.RequestExceptions as e:
+    print(e)
+
+todos_url = (
+    'https://jsonplaceholder.typicode.com/todos?userId=' +
+    sys.argv[1] +
+    '&completed=true')
+try:
+    # fetch data
+    todos_response = requests.get(todos_url)
+    # parse data
+    todos_data = todos_response.json()
+    # NUMBER_OF_DONE_TASKS
+    NUMBER_OF_DONE_TASKS = len(todos_data)
+except requests.RequestExceptions as e:
+    print(e)
+
+# print resaults
+print(f'Employee {EMPLOYEE_NAME} is done with tasks \
+({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+for todo in todos_data:
+    print(todo['title'])
