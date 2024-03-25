c12da397b17014699572f8dc3180157978659761
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-25 22:38:35 +0000

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,15 +6,24 @@ import sys
 
 
 if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + user_id
-    res = requests.get(url_user).json()
-    username = res.get("username")
-    req = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' +
-        (user_id) + '/todos')
-    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
-        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            writer.writerow([user_id, username,
-                            task.get("completed"), task.get("title")])
+    data_to_write = []
+    USER_ID = sys.argv[1]
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{USER_ID}/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{USER_ID}'
+    todos_data = requests.get(todos_url).json()
+    user_data = requests.get(user_url).json()
+    USERNAME = user_data.get('username')
+    TASK_COMPLETED_STATUS = []
+    TASK_TITLE = []
+
+    for todo in todos_data:
+        tmp = []
+        tmp.append(USER_ID)
+        tmp.append(USERNAME)
+        tmp.append(todo.get('completed'))
+        tmp.append(todo.get('title'))
+        data_to_write.append(tmp)
+
+    with open(f'{USER_ID}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        writer.writerows(data_to_write)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,16 +6,4 @@ import sys
 
 
 if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                         .format(user_id)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(sys.argv[1])).json()
-
-    with open("{}.json".format(user_id), "w") as user_id:
-        json.dump({user_id: [{
-                'task': task.get('title'),
-                'completed': task.get('completed'),
-                'username': users.get('username')
-            } for task in todos]}, user_id)
+    
