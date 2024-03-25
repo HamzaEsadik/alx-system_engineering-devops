576518c90eb8160943b2e1749df7f57c7188b5ca
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-03-25 23:01:58 +0000

    taks 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,8 +6,6 @@ import sys
 
 
 if __name__ == '__main__':
-    data_to_write = []
-    data = []
     USER_ID = sys.argv[1]
     todos_url = f'https://jsonplaceholder.typicode.com/users/{USER_ID}/todos'
     user_url = f'https://jsonplaceholder.typicode.com/users/{USER_ID}'
@@ -15,9 +13,9 @@ if __name__ == '__main__':
     user_data = requests.get(user_url).json()
     USERNAME = user_data.get('username')
 
-    with open(f'{USER_ID}.json') as json_file:
+    with open(f'{USER_ID}.json', 'w') as json_file:
         json.dump({USER_ID: [{
-            'task': todos_data.get('title'),
-            'completed': todos_data.get('completed'),
+            'task': todo.get('title'),
+            'completed': todo.get('completed'),
             'username': USERNAME
         } for todo in todos_data]}, json_file)
