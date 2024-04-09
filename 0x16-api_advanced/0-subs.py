05f3b58060c48ced7b9cbd46491b91f8bfcf135b
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-04-09 17:41:51 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Reddit Sub Counter."""
-    import requests
-
-    the_results = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "1-top_ten.py"},
-                            allow_redirects=False)
-    if the_results.status_code >= 300:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Linux:0x16 api.advanced:v1.0.0 (by u/nm01)'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return 0
-
-    return the_results.json().get("data").get("subscribers")
