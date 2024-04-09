8f00174b71ff5a75858ea507b141c8540ce50b14
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-04-09 13:48:46 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,16 @@
 """Script that queries subscribers"""
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    """Function that queries the Reddit API"""
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Mozilla/5.0"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
         return 0
