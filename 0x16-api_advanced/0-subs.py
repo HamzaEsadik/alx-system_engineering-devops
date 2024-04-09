e113cd4b82ca54d94e6a2019ea082b5d2411baa8
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-04-09 13:31:21 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Function to query subscribers"""
-import json
+"""Script that queries subscribers"""
+
 import requests
+import json
 
 
-def def number_of_subscribers(subreddit):
-    user = {"User-Agent": "Hamza_esadik"}
-    request = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit), headers=user)
-    if request.status_code == 200:
-        return request.json().get("data").get("subscribers")
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
