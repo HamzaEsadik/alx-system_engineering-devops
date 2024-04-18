2e8669bf562524eb71fcfcdf0b6eed949fac37cd
HamzaEsadik <hamza.esadik.m@gmail.com>
2024-04-18 17:51:57 +0100

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# bring limit higher
+exec {'fd':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/15/3000/g' /etc/default/nginx; sudo service nginx restart",
+provider => 'shell',
+}
